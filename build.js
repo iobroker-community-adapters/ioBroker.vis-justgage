40d44d7624ebbfd467af340dddf639eda7bb9c90
GermanBluefox <dogafox@gmail.com>
2024-03-07 12:43:17 +0800

    Added message if vis(-2) not installed

--- build.js
+++ build.js
@@ -4,7 +4,10 @@ const fs = require('node:fs');
 const pack = require('./package.json');
 
 const html = fs.readFileSync(`${__dirname}/widgets/justgage.html`, 'utf8');
-const newHtml = html.replace(/version: "\d+\.\d+\.\d+"/, `version: "${pack.version}"`);
+const newHtml = html
+    .replace(/version: "\d+\.\d+\.\d+"/, `version: "${pack.version}"`)
+    .replace(/version: '\d+\.\d+\.\d+'/, `version: '${pack.version}',`)
+    .replace(/version: "\d+\.\d+\.\d+",/, `version: "${pack.version}",`);
 if (html !== newHtml) {
     fs.writeFileSync(`${__dirname}/widgets/justgage.html`, newHtml);
     console.log('justgage.html updated');
@@ -13,7 +16,8 @@ if (html !== newHtml) {
 const js = fs.readFileSync(`${__dirname}/widgets/justgage/js/justgage.js`, 'utf8');
 const newJs = js
     .replace(/version: "\d+\.\d+\.\d+"/, `version: "${pack.version}"`)
-    .replace(/version: '\d+\.\d+\.\d+',/, `version: '${pack.version}',`);
+    .replace(/version: '\d+\.\d+\.\d+',/, `version: '${pack.version}',`)
+    .replace(/version: "\d+\.\d+\.\d+",/, `version: "${pack.version}",`);
 if (js !== newJs) {
     fs.writeFileSync(`${__dirname}/widgets/justgage/js/justgage.js`, newJs);
     console.log('justgage.js updated');
